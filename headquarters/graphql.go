c51d6ef0ef6e27536b76d2e7cdc362ab03d5d10a
Josh Menden <josh.menden@gmail.com>
2020-07-14 15:34:23 -0600

    Remove unecessary structs

--- headquarters/graphql.go
+++ headquarters/graphql.go
@@ -33,19 +33,6 @@ const schemaString = `
 	}
 `
 
-type Ship struct {
-	Name       string
-	Location   string
-	FuelLevel  string
-	Deliveries []Delivery
-}
-
-type Delivery struct {
-	Uuid             string
-	NumberOfPackages int32
-	DeliveryDate     string
-}
-
 type RootResolver struct{}
 
 func (r *RootResolver) Ship() (pb.Ship, error) {

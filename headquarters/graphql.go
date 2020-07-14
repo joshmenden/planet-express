f6fa73f77a17f4775b6eaee3198764c221b63bd2
Josh Menden <josh.menden@gmail.com>
2020-07-14 15:14:46 -0600

    Basic ship gql functional

--- headquarters/graphql.go
+++ headquarters/graphql.go
@@ -8,70 +8,62 @@ const schemaString = `
 	schema {
 		query: Query
 	}
-	type User {
-		userID: ID!
-		username: String!
-		emoji: String!
-		notes: [Note!]!
+	type Ship {
+		name: String!
+		location: String!
+		fuelLevel: FuelLevel!
+		deliveries: [Delivery!]!
 	}
 
-	type Note {
-		noteID: ID!
-		data: String!
+	type Delivery {
+		uuid: String!
+		// number_of_packages: Int
+		deliveryDate: String!
 	}
 
 	type Query {
-		users: [User!]!
+		ships: [Ship!]!
+	}
+
+	enum FuelLevel {
+		EMPTY
+		LOW
+		FULL
 	}
 `
 
-type User struct {
-	UserID   graphql.ID
-	Username string
-	Emoji    string
-	Notes    []Note
+type Ship struct {
+	Name       string
+	Location   string
+	FuelLevel  string
+	Deliveries []Delivery
 }
 
-type Note struct {
-	NoteID graphql.ID
-	Data   string
+type Delivery struct {
+	Uuid             string
+	NumberOfPackages int
+	DeliveryDate     string
 }
 
-var users = []User{
+var ships = []Ship{
 	{
-		UserID:   graphql.ID("u-001"),
-		Username: "nyxerys",
-		Emoji:    "🇵🇹",
-		Notes: []Note{
-			{NoteID: "n-001", Data: "Olá Mundo!"},
-			{NoteID: "n-002", Data: "Olá novamente, mundo!"},
-			{NoteID: "n-003", Data: "Olá, escuridão!"},
-		},
-	}, {
-		UserID:   graphql.ID("u-002"),
-		Username: "rdnkta",
-		Emoji:    "🇺🇦",
-		Notes: []Note{
-			{NoteID: "n-004", Data: "Привіт Світ!"},
-			{NoteID: "n-005", Data: "Привіт ще раз, світ!"},
-			{NoteID: "n-006", Data: "Привіт, темрява!"},
-		},
-	}, {
-		UserID:   graphql.ID("u-003"),
-		Username: "username_ZAYDEK",
-		Emoji:    "🇺🇸",
-		Notes: []Note{
-			{NoteID: "n-007", Data: "Hello, world!"},
-			{NoteID: "n-008", Data: "Hello again, world!"},
-			{NoteID: "n-009", Data: "Hello, darkness!"},
+		Name:      "test ship here",
+		Location:  "Lehi UT",
+		FuelLevel: "FULL",
+		Deliveries: []Delivery{
+			{
+				Uuid: "abc987654321",
+				// NumberOfPackages: 4,
+				DeliveryDate: "march 3 1990",
+			},
 		},
 	},
 }
 
 type RootResolver struct{}
 
-func (r *RootResolver) Users() ([]User, error) {
-	return users, nil
+func (r *RootResolver) Ships() ([]Ship, error) {
+	return ships, nil
 }
 
 var (
